0832d9ff089e6012a4c4954f4af80aafb4fd27dc
Julian Coronado <julian.c28@gmail.com>
2025-12-23 23:23:34 -0800

    style: refactor typography and font naming in theme settings

--- app/src/main/java/com/jcoronado/minimalbitcoinwidget/ui/theme/Type.kt
+++ app/src/main/java/com/jcoronado/minimalbitcoinwidget/ui/theme/Type.kt
@@ -11,33 +11,42 @@ val gFontsProvider = GoogleFont.Provider(
     providerPackage = "com.google.android.gms",
     certificates = R.array.com_google_android_gms_fonts_certs
 )
-val fontName = GoogleFont("Google Sans Code")
+val googleSansCode = GoogleFont("Google Sans Code")
 
-val googleSansFontFamily = FontFamily(
-    Font(googleFont = fontName, fontProvider = gFontsProvider)
+// val googleSansFlex = GoogleFont("Google Sans Flex")
+
+val googleSansCodeFontFamily = FontFamily(
+    Font(googleFont = googleSansCode, fontProvider = gFontsProvider)
 )
+
+// not sure if I want to use this font...
+// val googleSansFlexFontFamily = FontFamily(
+//     Font(googleFont = googleSansFlex, fontProvider = gFontsProvider)
+// )
+
+val currentFont = googleSansCodeFontFamily
 
 // Set of Material typography styles to start with
 val defaultTypography = Typography()
 
 val appTypography = Typography(
-    displayLarge = defaultTypography.displayLarge.copy(fontFamily = googleSansFontFamily),
-    displayMedium = defaultTypography.displayMedium.copy(fontFamily = googleSansFontFamily),
-    displaySmall = defaultTypography.displaySmall.copy(fontFamily = googleSansFontFamily),
+    displayLarge = defaultTypography.displayLarge.copy(fontFamily = currentFont),
+    displayMedium = defaultTypography.displayMedium.copy(fontFamily = currentFont),
+    displaySmall = defaultTypography.displaySmall.copy(fontFamily = currentFont),
 
-    headlineLarge = defaultTypography.headlineLarge.copy(fontFamily = googleSansFontFamily),
-    headlineMedium = defaultTypography.headlineMedium.copy(fontFamily = googleSansFontFamily),
-    headlineSmall = defaultTypography.headlineSmall.copy(fontFamily = googleSansFontFamily),
+    headlineLarge = defaultTypography.headlineLarge.copy(fontFamily = currentFont),
+    headlineMedium = defaultTypography.headlineMedium.copy(fontFamily = currentFont),
+    headlineSmall = defaultTypography.headlineSmall.copy(fontFamily = currentFont),
 
-    titleLarge = defaultTypography.titleLarge.copy(fontFamily = googleSansFontFamily),
-    titleMedium = defaultTypography.titleMedium.copy(fontFamily = googleSansFontFamily),
-    titleSmall = defaultTypography.titleSmall.copy(fontFamily = googleSansFontFamily),
+    titleLarge = defaultTypography.titleLarge.copy(fontFamily = currentFont),
+    titleMedium = defaultTypography.titleMedium.copy(fontFamily = currentFont),
+    titleSmall = defaultTypography.titleSmall.copy(fontFamily = currentFont),
 
-    bodyLarge = defaultTypography.bodyLarge.copy(fontFamily = googleSansFontFamily),
-    bodyMedium = defaultTypography.bodyMedium.copy(fontFamily = googleSansFontFamily),
-    bodySmall = defaultTypography.bodySmall.copy(fontFamily = googleSansFontFamily),
+    bodyLarge = defaultTypography.bodyLarge.copy(fontFamily = currentFont),
+    bodyMedium = defaultTypography.bodyMedium.copy(fontFamily = currentFont),
+    bodySmall = defaultTypography.bodySmall.copy(fontFamily = currentFont),
 
-    labelLarge = defaultTypography.labelLarge.copy(fontFamily = googleSansFontFamily),
-    labelMedium = defaultTypography.labelMedium.copy(fontFamily = googleSansFontFamily),
-    labelSmall = defaultTypography.labelSmall.copy(fontFamily = googleSansFontFamily)
+    labelLarge = defaultTypography.labelLarge.copy(fontFamily = currentFont),
+    labelMedium = defaultTypography.labelMedium.copy(fontFamily = currentFont),
+    labelSmall = defaultTypography.labelSmall.copy(fontFamily = currentFont)
 )
