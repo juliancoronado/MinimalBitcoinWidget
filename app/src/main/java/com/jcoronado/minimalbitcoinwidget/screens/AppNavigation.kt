d3f9dc902052073c4dcba504bf88bd5a26f23930
Julian Coronado <julian.c28@gmail.com>
2026-01-21 16:53:00 -0800

    build: remove unused Scaffold import in AppNavigation.kt

--- app/src/main/java/com/jcoronado/minimalbitcoinwidget/screens/AppNavigation.kt
+++ app/src/main/java/com/jcoronado/minimalbitcoinwidget/screens/AppNavigation.kt
@@ -32,7 +32,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.motionScheme
 import androidx.compose.material3.PlainTooltip
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.ToggleButton
 import androidx.compose.material3.ToggleButtonDefaults
